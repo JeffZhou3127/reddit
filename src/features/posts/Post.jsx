c1b019296a6e9b4d0ff50f6adbe3db8188adf0ce
Jeff Zhou <zhouj0805@gmail.com>
2025-10-02 18:42:36 -0700

    Render API data

--- src/features/posts/Post.jsx
+++ src/features/posts/Post.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import styles from "./Post.module.css";
 
-const Post = () => {
-  // Test code. Improve later
+const Post = ({ postImage, author, postDescription }) => {
+  // Uses props to generate a post
   return (
     <section className={styles.section}>
-      <img
-        src="https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8="
-        alt="Just went on vacation! The views are breathtaking."
-        className={styles.img}
-      />
+      <img src={postImage} alt={postDescription} className={styles.img} />
       <article className={styles.article}>
-        <p className={styles.author}>Posted by John</p>
-        <p>Just went on vacation! The views are breathtaking.</p>
+        <p className={styles.author}>Posted by {author}</p>
+        <p>{postDescription}</p>
       </article>
     </section>
   );
--- src/views/Homepage.jsx
+++ src/views/Homepage.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchPostData, selectPosts } from "../features/posts/postsSlice";
 import styles from "./Homepage.module.css"
 import Post from "../features/posts/Post";
 
 const Homepage = () => {
+  const posts = useSelector(selectPosts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPostData());
+  }, [dispatch]);
+
   return (
     <div className={styles.container}>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      {posts.map(({ PostImage, PostDescription, Author, id }) =>
+        <Post key={id} postImage={PostImage} postDescription={PostDescription} author={Author} />
+      )}
     </div>
   );
 };
